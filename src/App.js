4bfa0c2e2968029fde9ebc7e13f75e8a8657b45f
Candela Abril Turlione <abrilturlione@gmail.com>
2021-03-03 17:08:45 -0300

    Primera entrega

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primer desafío: crear app con el CLI
         </p>
         <a
           className="App-link"
