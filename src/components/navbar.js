dd15accef71a3fda95f4dd1dd19a68a0e761eeb6
Candela Abril Turlione <abrilturlione@gmail.com>
2021-03-10 14:01:45 -0300

    Entrega segunda clase

---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Efecto Tequila</a> 
+                </li>
+                <li>
+                    <a href="">Indumentaria</a>
+                </li>
+                <li>
+                    <a href="">Accesorios</a>
+                </li>
+                <li>
+                    <a href="">Bolsos</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
